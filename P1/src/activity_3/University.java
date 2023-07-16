9a54e2623fed651310248967f3da313041932d52
David Camacho Copa <david.camacho@jala.university>
2023-07-16 10:36:53 -0400

    activity 3, lab 2

---
+++ P1/src/activity_3/University.java
@@ -0,0 +1,12 @@
+package Activity3;
+
+import java.util.ArrayList;
+
+public class University {
+  private ArrayList<Student> students;
+
+  public void addStudent(Student student){
+    students.add(student);
+  }
+
+}
